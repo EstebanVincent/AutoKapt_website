ccd03582bb1f6427aec7388ab425ec795e991ceb
Esteban Vincent <este.vincent@gmail.com>
2021-04-09 18:23:48 +0200

    save header

--- bases/header.php
+++ bases/header.php
@@ -5,70 +5,89 @@
 
 <!DOCTYPE html>
 <html>
-
-<head>
-  <title>Infinite Mesure</title>
-  <link rel="stylesheet" href="/AutoKapt/style.css">
-  <link rel="icon" href="/AutoKapt/favicon3.ico" type="image/x-icon" />
-  <script src="https://kit.fontawesome.com/7f495e885c.js" crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-
-  <!-- <link rel="stylesheet" href="https://unpkg.com/ionicons@5.5.10-0/dist/css/ionicons.min.css"> -->
-</head>
-
+    <head>
+        <title>Infinite Mesure</title>
+        <link rel="stylesheet" href="/AutoKapt/CSS/styleHeader.css" />
+        <link rel="stylesheet" href="/AutoKapt/CSS/style.css" />
+        <link rel="icon" href="/AutoKapt/favicon3.ico" type="image/x-icon" />
+        <script src="https://kit.fontawesome.com/7f495e885c.js" crossorigin="anonymous"></script>
+        <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+        <!-- Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+    </head>
 <body>
-  <header>
-    <nav>
-      <ul>
-        <li class="hover" id="name"><a href="/AutoKapt/home.php">Infinite Mesure</a></li>
-        <?php
-        if(isset($_SESSION["userUsername"])){
-          if($_SESSION["userAccess"] == 0){
-            echo '<li class="hover scroll"><a href="#">' . $_SESSION["userUsername"] . '</a>';
-                    echo '<ul class="sous">';
-            echo '<li class="hover"><a href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>';
-            echo '<li class="hover"><a href="/AutoKapt/pages/Admin/createManager.php">Create Manager</a></li>';
-            echo '<li class="hover"><a href="/AutoKapt/pages/Admin/modifyFAQ.php">Update FAQ</a></li>';
-            echo '<li class="hover"><a href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>';
-          echo'</ul>';
-          echo '</li>';
-          } 
-          else if ($_SESSION["userAccess"] == 1){
-            echo '<li class="hover scroll"><a href="#">' . $_SESSION["userUsername"] . '</a>';
-            echo '<ul class="sous">';
-            echo '<li class="hover"><a href="/AutoKapt/pages/Manager/createUser.php">Create user</a></li>';
-            echo '<li class="hover"><a href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>';
-            echo '<li class="hover"><a href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>';
-            echo'</ul>';
-            echo '</li>';
-          }
-          else if ($_SESSION["userAccess"] == 2){
-            echo '<li class="hover scroll"><a href="#">' . $_SESSION["userUsername"] . '</a>';
-                echo '<ul class="sous">';
-                    echo '<li class="hover"><a href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>';
-                    echo '<li class="hover"><a href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>';
-                 echo'</ul>';
-            echo '</li>';
-          }
-        }
-        else {
-          echo '<li class="hover"><a href="/AutoKapt/pages/logIn/logIn.php">Log in</a></li>';
-        }
-        ?>
-        <li class="hover scroll"><a href="#">Langue</a>
-          <ul class="sous">
-            <li><a href="/AutoKapt/includes/language/change2English.php">English</a></li>
-            <li><a href="/AutoKapt/includes/language/change2French.php">Français</a></li>
-          </ul>
-        </li>
-        <li class="hover"><a href="/AutoKapt/pages/faq.php">FAQ</a></li>
-        <li class="hover"><a href="/AutoKapt/pages/team.php">L'équipe</a></li>
-        <li class="hover"><a href="/AutoKapt/pages/User/stress.php">Stress</a></li>
-        <li class="hover"><a href="/AutoKapt/pages/User/reflex.php">Reflexe</a></li>
-        <li><input  type="search" class="searchTerm" placeholder="Search">
-        </li>
-        
-      </ul>
-    </nav>
-    <div id="navbar"></div>
+    <header class="header">
+        <nav class="navbar navbar-style">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#micon">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <a href="/AutoKapt/home.php"><img src="/AutoKapt/images/logo.png" class="logo" /></a>
+                </div>
+                <div class="colapse navabar-collapse" id="micon">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="nav-item"><a href="/AutoKapt/pages/User/reflex.php">Reflexe</a></li>
+                        <li class="nav-item"><a href="/AutoKapt/pages/User/stress.php">Stress</a></li>
+                        <li class="nav-item"><a href="/AutoKapt/pages/faq.php">FAQ</a></li>
+                        <li class="nav-item"><a href="/AutoKapt/pages/team.php">L'equipe</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"> Langue </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="/AutoKapt/includes/language/change2English.php">English</a></li>
+                                <li><a class="dropdown-item" href="/AutoKapt/includes/language/change2French.php">Francais</a></li>
+                            </ul>
+                        </li>
+<?php
+                        if(isset($_SESSION["userUsername"])){
+                          if($_SESSION["userAccess"] == 0){
+                            echo 
+                            '
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">' . $_SESSION["userUsername"] . '</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/Admin/createManager.php">Create Manager</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/Admin/modifyFAQ.php">Update FAQ</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>
+                                </ul>
+                            </li>';
+                          } 
+                          else if($_SESSION["userAccess"] == 1){
+                            echo 
+                            '
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">' . $_SESSION["userUsername"] . '</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/Manager/createUser.php">Create User</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>
+                                </ul>
+                            </li>';
+                          } 
+                          else if($_SESSION["userAccess"] == 2){
+                            echo 
+                            '
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">' . $_SESSION["userUsername"] . '</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/myProfile.php">My profile</a></li>
+                                    <li><a class="dropdown-item" href="/AutoKapt/pages/profile/logOut.php">Log out</a></li>
+                                </ul>
+                            </li>';
+                          } 
+                          else {
+                            echo "<li class='nav-item'><a href='/AutoKapt/pages/logIn/logIn.php'>Log in</a></li>";
+                          }
+                        }
+?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+      </header>
+      
--- home.php
+++ home.php
@@ -3,38 +3,25 @@
   $pathHeader .= '/AutoKapt/bases/header.php'; /* psq le / va voir la vrai root d'ou cette méthode */
   include_once($pathHeader);
 ?>
-    <div id="imagePrincipale">
-      <h1>Infinite Mesure</h1>
-      <div id="firstLine"></div>
-      <h3>Notre projet</h3>
-    </div>
-  </header>
 
-  <section id="presentation">
-    <div id="txtIntro">
-      <h2>Une équipe, un projet</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Non, suscipit? Vitae culpa esse, animi voluptatibus voluptas
-        non cupiditate ea soluta quo. Distinctio culpa, fugit excepturi
-        id molestiae enim consequatur adipisci porro maiores, esse omnis
-        a, aspernatur iste illum tempora eaque?</p>
+<section class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <h1>Infinite Measures</h1>
+            <p>Home</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Non, suscipit? Vitae culpa esse, animi voluptatibus voluptas
+            non cupiditate ea soluta quo. Distinctio culpa, fugit excepturi
+            id molestiae enim consequatur adipisci porro maiores, esse omnis
+            a, aspernatur iste illum tempora eaque?</p>
+        </div>
+        <div class="col-sm-6">
+            <img src="/AutoKapt/images/background.jpg" class="img-responsive" />
+        </div>
     </div>
-    <div id="prestation">
-      <div class="imagesPresations">
-        <h4>Nous trouver</h4>
-        <a href="#"><img src="images/background.jpg" alt="background"></a>
-      </div>
-      <div class="imagesPresations">
-        <h4>Test</h4>
-        <a href="#"><img src="images/leaf.jpg" alt="leaf"></a>
-      </div>
-    </div>
-  </section>
-  <section id="passer le test">
+</section>
 
-  </section>
-
-  <?php
+<?php
   $pathFooter = $_SERVER['DOCUMENT_ROOT'];
   $pathFooter .= '/AutoKapt/bases/footer.php';/* psq le / va voir la vrai root d'ou cette méthode */
   include_once($pathFooter);
