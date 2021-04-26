c1a00d0d0339a6b8501ed557f21408c6b7cf5f5f
jpascal.vostatek@icloud.com <jpascal.vostatek@icloud.com>
2021-04-26 11:47:25 +0200

    Style testpage

--- bases/header.php
+++ bases/header.php
@@ -52,7 +52,8 @@
                     <div class="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul class="navbar-nav">
                             <li class="nav-item"><a class="nav-link" href="/AutoKapt/pages/User/dashboard.php">Dashboard</a></li>
-                            <li class="nav-item"><a class="nav-link" href="/AutoKapt/pages/team.php">L'equipe</a></li>
+                            <li class="nav-item"><a class="nav-link" href="/AutoKapt/pages/team.php">L'équipe</a></li>
+
 <?php
                       if(isset($_SESSION["userUsername"])){
                         if($_SESSION["userAccess"] == 0){
@@ -94,7 +95,8 @@
                           </li>';
                         } 
                       } else {
-                        echo "<li class='nav-item'><a class='nav-link' href='/AutoKapt/pages/logIn/logIn.php'>Log in</a></li>";
+                        echo' <li class="nav-item"><a class="nav-link" href="/AutoKapt/pages/testpage.php">Tests</a></li>
+                              <li class="nav-item"><a class="nav-link" href="/AutoKapt/pages/logIn/logIn.php">Log in</a></li>';
                       }
 ?>
                         </ul>
--- pages/testpage.php
+++ pages/testpage.php
@@ -8,23 +8,23 @@
 <div id = "imagePrincipale">
       <h1>APP G9E</h1>
       <div id = "firstLine"></div>
-      <h3>Test Page</h3>
+      <h3>Les différents tests</h3>
 
 
 
   <div class="testpagemain">
-
-    <div class="testpage">
+  
+    <div class="testpage rounded" >
       <img src="/AutoKapt/images/reflexe.png" alt="ImgReflexe" class="testpage-imgL"></img>
         <div>
-        <p class = "testpage-titre">Mesure des réflexes</p>
+        <p class = "testpage-titre">Mesure du stresse</p>
         <p class="testpage-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
          Fugit, animi. Delectus dolores nihil vero dolorum molestiae
          consequatur alias eaque aut.</p>
          </div>
     </div>
 
-    <div class="testpage">
+    <div class="testpage rounded">
         <div>
         <p class = "testpage-titre">Mesure des réflexes</p>
         <p class="testpage-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -34,16 +34,28 @@
          <img src="/AutoKapt/images/reflexe.png" alt="ImgReflexe" class="testpage-imgR"></img>
 
     </div>    
-    
-    <div class="testpage">
+
+    <div class="testpage rounded">
       <img src="/AutoKapt/images/reflexe.png" alt="ImgReflexe" class="testpage-imgL"></img>
         <div>
-        <p class = "testpage-titre">Mesure des réflexes</p>
+        <p class = "testpage-titre">Tests de mémoire</p>
         <p class="testpage-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
          Fugit, animi. Delectus dolores nihil vero dolorum molestiae
          consequatur alias eaque aut.</p>
          </div>
     </div>  
+
+    <div class="testpage rounded">
+        <div>
+        <p class = "testpage-titre">Tests d'audition</p>
+        <p class="testpage-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+         Fugit, animi. Delectus dolores nihil vero dolorum molestiae
+         consequatur alias eaque aut.</p>
+         </div>
+         <img src="/AutoKapt/images/reflexe.png" alt="ImgReflexe" class="testpage-imgR"></img>
+
+    </div> 
+   
   </div>
 
 
