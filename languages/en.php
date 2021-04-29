dcbc04603a95414fd4cf49919f7f7f70cfdfe4fe
jpascal.vostatek@icloud.com <jpascal.vostatek@icloud.com>
2021-04-29 14:01:47 +0200

    Traduction de la testpage

--- languages/en.php
+++ languages/en.php
@@ -44,5 +44,22 @@
         "nav-update-user" => "Update users",
         "nav-logout" => "Log out",
         "nav-login" => "Log in",
+
+        /* testpage */
+        "testdesc-stress-title" => "Stress measurment",
+        "testdesc-stress-desc" => "You will be asked to place your finger inside of an opaque tube which will determine your heart rate by transparency through your finger.
+        You will also be asked to pinch a temperature sensor between your fingers to inform the system of your current body temperature.
+         Those data will allow the system to determine your stress level.",
+        "testdesc-reflex-title" => "Reflexes measurment",
+        "testdesc-reflex-desc" => "Your reflexes will be determined with both an auditory and a visual stimulus.
+        The first will be done by means of a light signal, the second with a sound in your headphones.
+         You will need to press the button in front of you as soon as you hear or see the signal for the system to deduce your reaction time.",
+        "testdesc-memory-title" => "Memory test",
+        "testdesc-memory-desc" => "The memory test consists of a rhythm which will be shown to you by means of light or sound signals.
+        you will then need to play this rhythm using the button. You will thus obtain a score out of 100 depending on your rhythm’s fidelity to the initial one.",
+        "testdesc-audition-title" => "Hearing test",
+        "testdesc-audition-desc" => "The hearing test will send a note to your headphones,
+        you will need to reproduce this note into the microphone as closely as possible using your smartphone.
+        You will thus be assigned a score out of 100 depending on the fidelity to the initial note.",
     );
 ?>
--- languages/fr.php
+++ languages/fr.php
@@ -45,6 +45,21 @@
         "nav-logout" => "Deconnexion",
         "nav-login" => "Connexion",
         
-
+        /* testpage */
+        "testdesc-stress-title" => "Mesure du stresse",
+        "testdesc-stress-desc" => "Il vous sera demandé de placer votre doigt dans un tube opaque qui déterminera votre rythme cardiaque par transparence au travers dudit doigt.
+        Il vous sera également demandé de pincer un capteur de température entre vos doigts pour informer le système de votre température corporelle actuelle.
+         Ces données permettront ainsi de déterminer votre niveau de stress.",
+        "testdesc-reflex-title" => "Mesure des réflexes",
+        "testdesc-reflex-desc" => "Vos réflexes seront évalués face à des stimuli auditifs et visuels.
+        Les premiers seront effectués au moyen d’un signal lumineux, les deuxièmes avec un son dans votre casque.
+         Vous devrez ainsi appuyer sur le bouton devant vous dès que vous entendez ou voyez le signal pour que le système en déduise votre temps de réaction.",
+        "testdesc-memory-title" => "Tests de mémoire",
+        "testdesc-memory-desc" => "Le test de mémoire consiste en un rythme qui vous sera montré au moyen de signaux lumineux ou sonores,
+        vous devrez ensuite reproduire ce rythme à l’aide du bouton. Vous optiendrez ainsi un score sur 100 indiquant la fidélité au rythme de base.",
+        "testdesc-audition-title" => "Tests d'audition",
+        "testdesc-audition-desc" => "Le test d’audition enverra dans votre casque une note,
+        vous devrez ainsi reproduire cette note dans le microphone le plus fidèlement possible à l’aide de votre smartphone.
+        Il vous sera ainsi attribué un score sur 100 en fonction de la fidélité à la note initiale.",
     );
 ?>
