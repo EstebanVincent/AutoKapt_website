8ed2165019bd67c41c5c5520e6def7a7077dc57f
Sofyane-chraki <74062289+Sofyane-chraki@users.noreply.github.com>
2021-05-28 17:41:35 +0200

    Update dashboard.php

--- View/User/dashboard.php
+++ View/User/dashboard.php
@@ -88,12 +88,12 @@
                     <tr class="text-white-50">
                         <td><h5><?php echo $lang['dashboard-memory'] ?></h5></td>
                         <td><a href="/AutoKapt/View/User/play/p.memory.php"><button class="btn btn-danger"><i class="far fa-play-circle"></i> <?php echo $lang['play'] ?></button></a><a href="/AutoKapt/View/User/result/memory.php"><button class="btn btn-danger"><i class="fas fa-chart-line"></i> Stats</button></a></td>
-                        <td><?php echo $moyMemory ?></td>
+                        <td><?php echo $moyMemory.'/100' ?></td>
                     </tr>
                     <tr class="text-white-50">
                         <td><h5><?php echo $lang['dashboard-hearing'] ?></h5></td>
                         <td><a href="/AutoKapt/View/User/play/p.audition.php"><button class="btn btn-danger"><i class="far fa-play-circle"></i> <?php echo $lang['play'] ?></button></a><a href="/AutoKapt/View/User/result/audition.php"><button class="btn btn-danger"><i class="fas fa-chart-line"></i> Stats</button></a></td>
-                        <td><?php  echo $moyAudition  ?></td>
+                        <td><?php  echo $moyAudition.'/100'  ?></td>
                     </tr>
                 </tbody>
             </table>
