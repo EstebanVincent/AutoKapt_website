da194720d9ceec623473ad284017f8a9b6668a19
Esteban Vincent <este.vincent@gmail.com>
2021-05-28 11:21:18 +0200

    resolution conflit changement de langue et tableUsers

--- includes/language/change2French.php
+++ includes/language/change2French.php
@@ -1,8 +1,11 @@
 <?php
 /* on enleve la query avant de rediriger afin d'éviter d'avoir plusieurs query */
-$bits = explode('?',$_SERVER['HTTP_REFERER']);
-$redirect = $bits[0];
-echo $redirect;
+$url = $_SERVER['HTTP_REFERER'];
+$bool = str_contains($_SERVER['HTTP_REFERER'],'?');
 
-die(header("location: " . $redirect . "?lang=fr"));
+if($bool){
+    die(header("location: " . $url . "&lang=fr"));
+} else {
+    die(header("location: " . $url . "?lang=fr"));
+}
 ?>
